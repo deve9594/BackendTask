4551acbd24772861fb60890cef5629588f0bde0b
Devendra <deve9594@gmail.com>
2020-06-02 23:52:39 +0530

    changes added

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var port = process.env.port || 8085
 var app = require('./server');
-http.createServer(app.handleRequest).listen(8085);
-console.log("server is running on 8085")
+http.createServer(app.handleRequest).listen(port);
+//console.log("server is running on 8085")
