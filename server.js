7bb25e722b35559b09a57305998e03f57ddd3f5c
Ubuntu <deve9594@gmail.com>
2020-06-03 20:27:49 +0000

    chages new database credential

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ module.exports = {
                         eid = q.e_id;
                         
                         pool.query("select employee.empid,ename,productid,orderid,total*quantity Amount from employee,sales where employee.empid='"+eid+"' and employee.empid=sales.empid;", function (err, result) {
-                        if (err) throw err;
+                        if (err) {
+				console.log("error",err);
+				throw err;
+			}
                         res.writeHead(200,{"content-type":"text/html"},result.rows);
                         res.write("<label for='eid'>Employee Id:</label><br><br><input type='text' id='eid' name='e_id' value='"+result.rows[0].empid+"'><br><br><label for='eame'>Employee Name:</label><br><br><input type='text' id='ename' name='e_name' value='"+result.rows[0].ename+"'><br><br><label for='pid'>Product ID:</label><br><br><input type='text' id='pid' name='p_id' value='"+result.rows[0].productid+"'><br><br><label for='oid'>Order ID:</label><br><br><input type='text' id='oid' name='o_id' value='"+result.rows[0].orderid+"'><br><br><label for='amt'>Amount:</label><br><br><input type='text' id='amt' name='amt' value='"+result.rows[0].amount+"'><br><br>");
                         console.log(result.rows[0].ename);
